32ffc18449cd35fe09a202cae712bbacfdf017d0
Xanderux <kmignot@hotmail.fr>
2025-05-23 10:57:57 +0200

    introduce TestStringToARMinstruction

--- tests/polymorph_test.go
+++ tests/polymorph_test.go
@@ -6,6 +6,42 @@ import (
 	"github.com/Xanderux/polymorph-ARM/sources"
 )
 
+func TestStringToARMinstruction(t *testing.T) {
+	tests := []struct {
+		input_string string
+		expected     sources.ARMinstruction
+	}{
+		{
+			input_string: "adds r7, #3",
+			expected: sources.ARMinstruction{
+				Mnemonic: "adds",
+				Operands: []string{"r7", "#3"},
+			},
+		},
+		{
+			input_string: "adds r0, #14",
+			expected: sources.ARMinstruction{
+				Mnemonic: "adds",
+				Operands: []string{"r0", "#14"},
+			},
+		},
+	}
+
+	for i, test := range tests {
+		result := sources.StringToARMinstruction(test.input_string)
+
+		if result.Mnemonic != test.expected.Mnemonic {
+			t.Errorf("Test %d: Expected mnemonic %s, got %s", i, test.expected.Mnemonic, result.Mnemonic)
+		}
+
+		for j := range test.expected.Operands {
+			if result.Operands[j] != test.expected.Operands[j] {
+				t.Errorf("Test %d: Expected operand %d to be %s, got %s", i, j, test.expected.Operands[j], result.Operands[j])
+			}
+		}
+	}
+}
+
 func TestIsARMInstruction(t *testing.T) {
 	armIns := []string{
 		"subs r1, r1, r1",
@@ -119,6 +155,14 @@ func TestPolymorphToInstruction(t *testing.T) {
 				Operands: []string{"r3", "#0"},
 			},
 			expected: "EORS r3 r3 r3",
+		},
+		{
+			input_string: "ADDS R7 $imm0",
+			input_inst: sources.ARMinstruction{
+				Mnemonic: "ADDS",
+				Operands: []string{"r7", "#3"},
+			},
+			expected: "ADDS R7 #3",
 		},
 	}
 
