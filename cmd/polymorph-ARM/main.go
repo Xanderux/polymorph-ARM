cf60bfddef166d0dc1b9328e849a82c8663cb40a
Xanderux <kmignot@hotmail.fr>
2025-06-06 12:58:48 +0200

    remove uncessary code

--- cmd/polymorph-ARM/main.go
+++ cmd/polymorph-ARM/main.go
@@ -8,14 +8,6 @@ import (
 	"github.com/Xanderux/polymorph-ARM/sources"
 )
 
-func displayARMinstruction(message string, arm sources.ARMinstruction) {
-	fmt.Print(message, " ", arm.Mnemonic, " ", arm.Operands[0], " ", arm.Operands[1])
-	if len(arm.Operands) == 3 {
-		fmt.Print(" ", arm.Operands[2])
-	}
-	fmt.Println()
-}
-
 func main() {
 	input := flag.String("i", "", "ARM assembly source file")
 	output := flag.String("o", "", "ARM assembly output file")
